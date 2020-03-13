d8fb8b46586f0e0a1ed49eebd531f55da457452d
olin ariel <61973635+arieloatwix@users.noreply.github.com>
2020-03-13 16:27:16 +0200

    Now it re-render, butwe need to create separate keys for each row

--- mashkant/src/MyTable.js
+++ mashkant/src/MyTable.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+//import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -11,82 +11,75 @@ import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import Fab from '@material-ui/core/Fab';
-
 import AddIcon from '@material-ui/icons/Add';
 
 
 
-
-const useStyles = makeStyles({
-  table: {
-
-  },
-});
+/* const useStyles = makeStyles({ 
+  
+});*/
 
 
 
 const Row = () => {
   return(
-    <TableRow>
+    <TableRow key={Math.random()}>
       <TableCell>
       <Select labelId="label" id="select" value="20">
-  <MenuItem value="10">Ten</MenuItem>
+  <MenuItem id="standard-basic1" value="10">Ten</MenuItem>
   <MenuItem value="20">Twenty</MenuItem>
 </Select>  
 </TableCell>
-      <TableCell><TextField id="standard-basic" label="orel" /> </TableCell> 
-      <TableCell><TextField id="standard-basic" label="Standard" /> </TableCell>
-      <TableCell><TextField id="standard-basic" label="Standard" /> </TableCell> 
-      <TableCell><TextField id="standard-basic" label="Standard" /> </TableCell> 
+      <TableCell><TextField key={Math.random()} id={Math.random()+ "standard-basic1"} label="orel"/> </TableCell> 
+      <TableCell><TextField key={Math.random()} id={Math.random()+ "standard-basic1"} label="Standard"/> </TableCell>
+      <TableCell><TextField key={Math.random()} id={Math.random()+ "standard-basic1"} label="Standard"/> </TableCell> 
+      <TableCell><TextField key={Math.random()} id={Math.random()+ "standard-basic1"} label="Standard"/> </TableCell> 
      </TableRow>
 )
 }
 
 export default function MyTable() {
-
+  
+  const [rows, setRows]=useState([0])
+  useEffect(()=>{console.log(rows)},[rows]);
  
+   function handleAdd(){
 
-   
 
-  const [rows, setRows]=useState([1])
-
-  const handleAdd=()=>{
-
-    let tempRows=rows; 
     let newRow=1; 
-    tempRows.push(newRow); 
-    setRows(tempRows)
-  return(rows)
+    
+    setRows([...rows, newRow])
+      return(rows)
     }
 
-useEffect(()=>{console.log(rows)},[rows]);
 
-  const classes = useStyles();
 
+ /*  const classes = useStyles(); */
+  
   return (
+    
     <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
+      <Table  aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>Sum</TableCell>
-          <TableCell >route</TableCell>
+            <TableCell >route</TableCell>
             <TableCell >period</TableCell>
             <TableCell >interest</TableCell>
             <TableCell >Madad</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-     {rows.map( (row)=>(
+
+     {rows.map((row)=>(
      <Row key = {row}></Row>
      )
      )}
      
-     
-        
         </TableBody>
       </Table>
-      <Fab color="primary" aria-label="add" onClick={handleAdd}>
-        <AddIcon />
+      <Fab color="primary" aria-label="add" >
+        <AddIcon onClick={handleAdd} />
       </Fab>
     </TableContainer>
   );
