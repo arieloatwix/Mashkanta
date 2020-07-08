190946e7348f0e7e0bb5a63a375313150160364b
kadoshoz <orelkadosh21@gmail.com>
2020-07-08 20:22:18 +0300

    Update functions.js

--- mashkant/src/functions.js
+++ mashkant/src/functions.js
@@ -19,128 +19,93 @@ export function PMT(ir, np, pv, fv) {
   fv || (fv = 0);
   let type = 0;
 
-  if (ir === 0) return -(pv + fv) / np;
+  if (ir === 0) return -pv / np;
   else ir *= 0.01 / 12;
   pvif = Math.pow(1 + ir, np);
-
-  pmt = (-ir * pv * (pvif + fv)) / (pvif - 1);
-
-  return -pmt;
+  console.log("pvif", pvif);
+  pmt = (ir * pv * pvif) / (pvif - 1);
+  console.log("pmt", pmt);
+  return pmt;
 }
 
-/* export function monthlyReturns(ir, np, pv, fv, type) {
-  let pmt = null;
-  let monthsArray = Array.from(Array(parseInt(np)).keys());
 
-  for (let i in monthsArray) {
-    if (pmt) {
-      np = np - 1;
-      pv = pv - pmt + ir * (0.01 / 12);
-    }
-    pmt = PMT(ir, np, pv, fv, type);
-    monthsArray[i] = pmt;
-    console.log(i, monthsArray[i]);
-  }
-  let sum = 0;
-  for (var j = 0; j < monthsArray.length; j++) {
-    sum += monthsArray[j];
-  }
-} */
 export function monthlyReturns(ir, np, pv, fv, madad, type) {
   const inflation = data("inflation");
+  let pmt = 0;
+  let monthsArray = Array.from(Array(parseInt(np)).keys());
+  let sum = 0;
+  pv = parseInt(pv);
 
   switch (type) {
     case 0: //prime
       {
         const prime = data("prime");
-        let pmt = 0;
-        let monthsArray = Array.from(Array(parseInt(np)).keys());
-
+        let gape = ir - prime[0].value;
+        let primeCurrent = 0;
         for (let i in monthsArray) {
-          let primeCurrent = (i === 0 && i) || prime[i].value;
-          console.log("pc", primeCurrent);
           if (i != 0) {
             pv = pv - (pmt - (primeCurrent * pv) / 1200);
           }
-          pmt = PMT(prime[i].value, np - i, pv, fv);
+          primeCurrent = (i === 0 && i) || (prime[i].value + gape);
+          console.log("pv in case", pv);
+          pmt = PMT(primeCurrent, np - i, pv, fv);
           monthsArray[i] = pmt;
+          sum += monthsArray[i];
         }
-        let sum = 0;
-        for (var j = 0; j < monthsArray.length; j++) {
-          if (monthsArray) sum += monthsArray[j];
-        }
-        console.log(sum);
       }
       break;
+
     case 1: // Mishtana Tsmuda
       {
-        let pmt;
-        let monthsArray = Array.from(Array(parseInt(np)).keys());
-        let sum = 0;
         let basicz = data("basicZ");
         let k;
+        let gape = ir - basicz[0].value;
         for (let i in monthsArray) {
-          k = Math.round(i / 60);
-          ir = basicz[k].value;
-          monthsArray[k] = pmt;
           madad = parseInt(data("inflation")[i].value);
-          pv = parseInt(pv);
-          pv += (pv * madad) / 100;
+          k = Math.floor(i / 60);
+          ir = (basicz[k].value + gape);
+          pmt = PMT(ir, np - i, pv, fv);
+          pv = pv + (pv * madad) / 1200 - (pmt - (ir * pv) / 1200);
+          monthsArray[i] = pmt;
           sum += monthsArray[i];
-          pmt = PMT(ir, np, pv, fv);
         }
-
-        console.log(sum);
       }
       break;
+
     case 2: // Mishtana Lo Tsmuda
       {
-        let pmt;
-        let monthsArray = Array.from(Array(parseInt(np)).keys());
-        let basicz = parseInt(data("basicZ"));
+        let basicLoz = data("basicLoZ");
+        let k;
+        let gape = ir - basicLoz[0].value;
         for (let i in monthsArray) {
-          ir = basicz[i / 60];
-          pmt = PMT(ir, np, pv, fv, madad, type);
+          k = Math.floor(i / 60);
+          ir = (basicLoz[k].value + gape);
+          pmt = PMT(ir, np - i, pv, fv);
+          pv = pv - (pmt - (ir * pv) / 1200);
           monthsArray[i] = pmt;
+          sum += monthsArray[i];
         }
-        let sum = 0;
-        for (var j = 0; j < monthsArray.length; j++) {
-          sum += monthsArray[j];
-        }
-        console.log(sum);
       }
       break;
-    case 3:
-      {
-        let pmt;
-        let monthsArray = Array.from(Array(parseInt(np)).keys());
 
+    case 3:// const Tsamud
+      {
         for (let i in monthsArray) {
-          pmt = PMT(ir, np, pv, fv, madad, type);
+          pmt = PMT(ir, np - i, pv, fv);
+          pv = pv + (pv * madad) / 1200 - (pmt - (ir * pv) / 1200);
           monthsArray[i] = pmt;
+          sum += monthsArray[i];
         }
-        let sum = 0;
-        for (var j = 0; j < monthsArray.length; j++) {
-          sum += monthsArray[j];
-        }
-        console.log(sum);
       }
       break;
-    case 4:
-      {
-        let pmt;
-        let monthsArray = Array.from(Array(parseInt(np)).keys());
 
-        for (let i in monthsArray) {
-          pmt = PMT(ir, np, pv, fv, madad, type);
-          monthsArray[i] = pmt;
-        }
-        let sum = 0;
-        for (var j = 0; j < monthsArray.length; j++) {
-          sum += monthsArray[j];
-        }
-        console.log(sum);
+    case 4:// const Lo Tsmuda
+      {
+        pmt = PMT(ir, np, pv, fv);
+        sum = pmt * np;
       }
       break;
   }
+
+  console.log("sum", sum);
 }
